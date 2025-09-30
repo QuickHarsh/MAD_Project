987fb174c474ba373fe86668faee5417f92f9edb
Harsh Patel <hp1659414@gmail.com>
2025-10-01 01:46:45 +0530

    added the Librabry and its Navigations

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { AudioProvider } from './src/context/AudioProvider';
+import RootNavigator from './src/navigations';
 
 export default function App() {
   return (
     <AudioProvider>
       <SafeAreaView>
         <StatusBar style="auto"/>
+        <RootNavigator/>
       </SafeAreaView>
     </AudioProvider>
   );
--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
+import HomeScreen from '../screens/HomeScreen';
+import PlayerScreen from '../screens/MusicPlayer';
+import LibraryScreen from '../screens/Library';
 
-import HomeScreen from "../screens/HomeScreen";
-import LibraryScreen from "../screens/Library";
-import PlayerScreen from "../screens/MusicPlayer";
+const Tab = createBottomTabNavigator();
+
+export default function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            let iconName = 'home';
+            if (route.name === 'Home') iconName = 'home';
+            if (route.name === 'Library') iconName = 'library';
+            if (route.name === 'Player') iconName = 'play-circle';
+
+            const map = {
+              home: 'home-outline',
+              library: 'musical-notes-outline',
+              'play-circle': 'play-circle-outline',
+            };
+            return <Ionicons name={map[iconName]} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Library" component={LibraryScreen}/>
+        <Tab.Screen name="Player" component={PlayerScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
